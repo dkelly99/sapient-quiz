1c2b09efbcafb870f6702a0eb7311a1977ae709e
Declan Kelly <declan.kelly@btinternet.com>
2018-07-23 07:59:40 +0100

    add more tests

--- src/store/reducers/questionData.test.js
+++ src/store/reducers/questionData.test.js
@@ -1,12 +1,14 @@
 // import {fromJS, Map, Set} from 'immutable';
 import {questionData} from './questionData';
-import {FETCH_QUESTIONS} from '../actions/questions';
+import {FETCH_QUESTIONS, UPDATE_NAME, UPDATE_SCOREBOARD} from '../actions/questions';
 
 const expectedInitialState = {
     isFetching: false,
     isSubmitting: false,
     questions: [],
-    name: ''
+    name: '',
+    scoreboard: [],
+    result: null
 };
 
 describe('questionData', () => {
@@ -29,17 +31,71 @@ describe('questionData', () => {
         });
     });
 
-
-    /*describe('When we add an object to a SET twice', () => {
-        it('Then it should only exist once in the SET', () => {
-            const obj: any = {test: 123};
-            const state: { [key: string]: Set<any>} = {
-                mySet: Set([fromJS(obj)])
+    describe('When UPDATE_NAME action is dispatched', () => {
+        it('Then state.name should be updated', () => {
+            const action = {
+                name: 'Declan Kelly',
+                type: UPDATE_NAME
             };
-            state.mySet.add(fromJS({test: 456}));
-            console.dir({mySet: state.mySet.toJS()});
 
-            expect(state.mySet.toJS()).toEqual([{test: 123}]);
+            const newState = questionData(undefined, action);
+            expect(newState.name).toEqual('Declan Kelly');
         });
-    });*/
+    });
+    describe('When UPDATE_SCOREBOARD action is dispatched', () => {
+        it('Then state.scoreboard should be updated', () => {
+            const mockScoreboardData = [{name: 'John Brown', score: 32}];
+            const action = {
+                scoreboard: mockScoreboardData,
+                type: UPDATE_SCOREBOARD
+            };
+
+            const newState = questionData(undefined, action);
+            expect(newState.scoreboard).toEqual(mockScoreboardData);
+        });
+        it('Then state.result should be set to NULL', () => {
+            const mockScoreboardData = [{name: 'John Brown', score: 32}];
+            const action = {
+                scoreboard: mockScoreboardData,
+                type: UPDATE_SCOREBOARD
+            };
+
+            const initialState = {
+                isFetching: false,
+                isSubmitting: false,
+                questions: [],
+                name: '',
+                scoreboard: [],
+                result: {name: 'Declan', score: 50}
+            };
+            const newState = questionData(initialState, action);
+            expect(newState.result).toEqual(null);
+        });
+        it('Then state.questions should be set to empty', () => {
+            const mockScoreboardData = [{name: 'John Brown', score: 32}];
+            const action = {
+                scoreboard: mockScoreboardData,
+                type: UPDATE_SCOREBOARD
+            };
+            const initialState = {
+                isFetching: false,
+                isSubmitting: false,
+                questions: [{
+                    id: "93-612-2232",
+                    title: "Praesent id massa id nisl venenatis lacinia?",
+                    answers: [
+                        {"value": "a", "text": "Mauris ullamcorper purus sit amet nulla."},
+                        {"value": "b", "text": "Suspendisse ornare consequat lectus."},
+                        {"value": "c", "text": "Suspendisse ornare consequat lectus."}
+                    ],
+                    correctAnswer: "a"
+                }],
+                name: '',
+                scoreboard: [],
+                result: {name: 'Declan', score: 50}
+            };
+            const newState = questionData(initialState, action);
+            expect(newState.questions).toEqual([]);
+        });
+    });
 });
